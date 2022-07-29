81c7885519f3fe3ea5ab4282f8b584ffe5bcf9fa
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-29 20:19:28 +0000

    Add functions to handle moving left, up, and down. Set intervals for respective keydown events. #5

--- main.js
+++ main.js
@@ -11,21 +11,41 @@ var $img = document.querySelector('img');
 var intervalID;
 function handleKeyDown(event) {
   if (event.code === 'ArrowUp') {
+    intervalID = clearInterval(intervalID);
     data.direction = 'north';
-    $img.className = data.direction;
+    data.start = true;
+    turn(data.direction);
+    intervalID = setInterval(moveUp, 16);
   } else if (event.code === 'ArrowLeft') {
+    intervalID = clearInterval(intervalID);
     data.direction = 'west';
-    $img.className = data.direction;
+    data.start = true;
+    turn(data.direction);
+    intervalID = setInterval(moveLeft, 16);
   } else if (event.code === 'ArrowDown') {
+    intervalID = clearInterval(intervalID);
     data.direction = 'south';
-    $img.className = data.direction;
+    data.start = true;
+    turn(data.direction);
+    intervalID = setInterval(moveDown, 16);
   } else if (event.code === 'ArrowRight') {
+    intervalID = clearInterval(intervalID);
     data.direction = 'east';
-    $img.className = data.direction;
+    data.start = true;
+    turn(data.direction);
+    intervalID = setInterval(moveRight, 16);
   } else if (event.code === 'Space') {
     if (data.start === false) {
       data.start = true;
-      intervalID = setInterval(moveRight, 16);
+      if (data.direction === 'south') {
+        intervalID = setInterval(moveDown, 16);
+      } else if (data.direction === 'north') {
+        intervalID = setInterval(moveUp, 16);
+      } else if (data.direction === 'east') {
+        intervalID = setInterval(moveRight, 16);
+      } else if (data.direction === 'west') {
+        intervalID = setInterval(moveLeft, 16);
+      }
     } else {
       data.start = false;
       intervalID = clearInterval(intervalID);
@@ -35,8 +55,22 @@ function handleKeyDown(event) {
 
 document.addEventListener('keydown', handleKeyDown);
 
+function turn(direction) {
+  $img.className = direction;
+}
+
 function moveRight() {
   $img.style.left = data.location.x++ + 'rem';
 }
 
-clearInterval(intervalID);
+function moveLeft() {
+  $img.style.left = data.location.x-- + 'rem';
+}
+
+function moveDown() {
+  $img.style.top = data.location.y++ + 'rem';
+}
+
+function moveUp() {
+  $img.style.top = data.location.y-- + 'rem';
+}
