d7b74381908406e72de945f5af8e40a8e3250ba3
Alice Lee <alicejeeuncodes@gmail.com>
2022-07-29 18:16:27 +0000

    Listen for key down events on document to rotate car. #2

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ var data = {
   direction: 'east'
 };
 
+var $img = document.querySelector('img');
+
 function handleKeyDown(event) {
   if (event.code === 'ArrowUp') {
     data.direction = 'north';
+    $img.className = data.direction;
   } else if (event.code === 'ArrowLeft') {
     data.direction = 'west';
+    $img.className = data.direction;
   } else if (event.code === 'ArrowDown') {
     data.direction = 'south';
+    $img.className = data.direction;
   } else if (event.code === 'ArrowRight') {
     data.direction = 'east';
+    $img.className = data.direction;
   }
 }
 
